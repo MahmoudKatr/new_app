0eed6cfee05bd7a1eca741841f473f3fd848a7c7
mahmoudhossam <mahmoud.modo45@gmail.com>
2025-02-05 20:51:36 +0200

    Adding URL✅

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -31,7 +31,7 @@ class Articles {
   String? author;
   String? title;
   String? description;
-
+  String? url;
   String? urlToImage;
   String? publishedAt;
   String? content;
@@ -40,6 +40,7 @@ class Articles {
       {this.author,
       this.title,
       this.description,
+      this.url,
       this.urlToImage,
       this.publishedAt,
       this.content});
@@ -48,6 +49,7 @@ class Articles {
     author = json['author'];
     title = json['title'];
     description = json['description'];
+    url = json['url'];
     urlToImage = json['urlToImage'];
     publishedAt = json['publishedAt'];
     content = json['content'];
@@ -58,6 +60,7 @@ class Articles {
     data['author'] = author;
     data['title'] = title;
     data['description'] = description;
+    data['url'] = url;
     data['urlToImage'] = urlToImage;
     data['publishedAt'] = publishedAt;
     data['content'] = content;
